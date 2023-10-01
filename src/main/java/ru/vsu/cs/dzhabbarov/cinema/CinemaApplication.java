979df50b49df064425da2ce3f96e69bf5f566414
Rafig <rafigdzabbarov0410@gmail.com>
2023-10-01 10:02:00 +0300

    check

--- src/main/java/ru/vsu/cs/dzhabbarov/cinema/CinemaApplication.java
+++ src/main/java/ru/vsu/cs/dzhabbarov/cinema/CinemaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CinemaApplication {
-
+	//TODO
 	public static void main(String[] args) {
 		SpringApplication.run(CinemaApplication.class, args);
 	}
