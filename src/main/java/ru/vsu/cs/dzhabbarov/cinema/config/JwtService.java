2354ba9d066afd49dc582b3b3a61309dd106968e
Rafig <rafigdzabbarov0410@gmail.com>
2023-11-03 19:51:21 +0300

    JwtService

---
+++ src/main/java/ru/vsu/cs/dzhabbarov/cinema/config/JwtService.java
@@ -0,0 +1,33 @@
+package ru.vsu.cs.dzhabbarov.cinema.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+
+@Service
+public class JwtService {
+    @Value("${application.security.jwt.secret-key}")
+    private static String secretKey;
+
+    public String extractUsername(String token) {
+        return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
