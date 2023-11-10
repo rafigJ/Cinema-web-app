44902b7ae42461a30c64495121b2151c0e51977f
Rafig <rafigdzabbarov0410@gmail.com>
2023-11-10 12:18:23 +0300

    frontend navbar continue

--- frontend/src/components/UI/Navbar/Navbar.jsx
+++ frontend/src/components/UI/Navbar/Navbar.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from '../img/logo.png';
-import userLogo from '../img/user.png';
+import logo from './img/logo.png';
+import userLogo from './img/user.png';
 import {Link} from "react-router-dom";
-import classes from "./Navbar.module.css"
 import NavbarElement from "./NavbarElement";
+import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <div className={classes.header}>
+        <div className="header">
             <Link to="/">
                 <img src={logo} width="239px" height="50px" alt=""/>
             </Link>
-            <nav className={classes.nav}>
+            <nav className="nav">
                 <ul>
                     <NavbarElement to="/">Главная</NavbarElement>
                     <NavbarElement to="/films">Фильмы</NavbarElement>
                     <NavbarElement to="/users">Другое</NavbarElement>
                 </ul>
             </nav>
-            <div className={classes.right_nav}>
-                <button className={classes.btn}>
+            <div className="right_nav">
+                <button className="btn">
                     <img src={userLogo} alt=''/>
                     <span >Войти</span>
                 </button>
