17be600a1565ca2a4fd43ce7b6a580810d61bbc2
Rafig <rafigdzabbarov0410@gmail.com>
2023-11-12 21:20:03 +0300

    frontend added React portal to properly "prioritize" the modal window display

--- frontend/src/components/UI/Modal/Modal.jsx
+++ frontend/src/components/UI/Modal/Modal.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import ReactDOM from "react-dom";
 import "./modal.css"
 
 const Modal = ({active, setActive, children}) => {
-    return (
-        <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)}>
-            <div className={active ? "modal__content active" : "modal__content"} onClick={e => e.stopPropagation()}>
+    return ReactDOM.createPortal(
+        <div className={active ? 'modal active' : 'modal'} onClick={() => setActive(false)}>
+            <div className={active ? 'modal__content active' : 'modal__content'} onClick={(e) => e.stopPropagation()}>
                 {children}
             </div>
-        </div>
+        </div>,
+        document.getElementById('modal-root')
     );
 };
 
