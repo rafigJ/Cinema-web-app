353d038bc93b9da184433d75e0b7e8c8858a11ad
Rafig <rafigdzabbarov0410@gmail.com>
2023-11-12 21:34:40 +0300

    frontend continue work with authForm

--- frontend/src/components/Authorization/AuthForm.jsx
+++ frontend/src/components/Authorization/AuthForm.jsx
@@ -6,6 +6,14 @@ const AuthForm = ({setModalActive}) => {
     const {setIsAuth} = useContext(AuthContext);
     const [isLoginForm, setIsLoginForm] = useState(true);
 
+    const [loginEmail, setLoginEmail] = useState('');
+    const [loginPassword, setLoginPassword] = useState('');
+
+    const [regEmail, setRegEmail] = useState('');
+    const [regPassword, setRegPassword] = useState('');
+    const [name, setName] = useState('');
+    const [lastName, setLastName] = useState('');
+
     const login = event => {
         event.preventDefault();
         setIsAuth(true);
@@ -27,8 +35,16 @@ const AuthForm = ({setModalActive}) => {
             <div className="container">
                 <h1>Вход</h1>
                 <form className="form">
-                    <input type="email" placeholder="Эл.почта"/>
-                    <input type="password" placeholder="Пароль"/>
+                    <input value={loginEmail}
+                           type="email"
+                           placeholder="Эл.почта"
+                           onChange={e => setLoginEmail(e.target.value)}
+                    />
+                    <input value={loginPassword}
+                           type="password"
+                           placeholder="Пароль"
+                           onChange={e => setLoginPassword(e.target.value)}
+                    />
                     <button onClick={login}>Войти</button>
                     <button className="secondary-btn" onClick={deactivateLoginForm}>Зарегистрироваться</button>
                 </form>
@@ -40,10 +56,30 @@ const AuthForm = ({setModalActive}) => {
         <div className="container">
             <h2>Создать аккаунт</h2>
             <form className="form">
-                <input type="text" placeholder="Имя"/>
-                <input type="text" placeholder="Фамилия"/>
-                <input type="email" placeholder="Эл.почта"/>
-                <input type="password" placeholder="Пароль"/>
+                <input
+                    value={name}
+                    type="text"
+                    placeholder="Имя"
+                    onChange={e => setName(e.target.value)}
+                />
+                <input
+                    value={lastName}
+                    type="text"
+                    placeholder="Фамилия"
+                    onChange={e => setLastName(e.target.value)}
+                />
+                <input
+                    value={regEmail}
+                    type="email"
+                    placeholder="Эл.почта"
+                    onChange={e => setRegEmail(e.target.value)}
+                />
+                <input
+                    value={regPassword}
+                    type="password"
+                    placeholder="Пароль"
+                    onChange={e => setRegPassword(e.target.value)}
+                />
                 <button onClick={login}>Создать</button>
                 <button className="secondary-btn" onClick={activateLoginForm}>Уже есть аккаунт</button>
             </form>
