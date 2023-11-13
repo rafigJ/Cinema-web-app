9f844859bac67c90cb9e2f95c98d1d60b0ae59e4
Rafig <rafigdzabbarov0410@gmail.com>
2023-11-13 16:48:45 +0300

    frontend/backend FilmService.js fix

--- backend/src/main/java/ru/vsu/cs/dzhabbarov/cinema/config/SecurityConfig.java
+++ backend/src/main/java/ru/vsu/cs/dzhabbarov/cinema/config/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import ru.vsu.cs.dzhabbarov.cinema.auth.UserAuthenticationEntryPoint;
 
 @Configuration
@@ -38,5 +40,16 @@ public class SecurityConfig {
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 }
