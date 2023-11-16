3abe52aadc1d54ab9605ee3e2511b624b26fa3bf
Rafig <rafigdzabbarov0410@gmail.com>
2023-11-17 01:43:45 +0300

    backend some fix

--- backend/src/main/java/ru/vsu/cs/dzhabbarov/cinema/service/FilmService.java
+++ backend/src/main/java/ru/vsu/cs/dzhabbarov/cinema/service/FilmService.java
@@ -1,27 +1,13 @@
 package ru.vsu.cs.dzhabbarov.cinema.service;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Service;
 import ru.vsu.cs.dzhabbarov.cinema.entity.FilmEntity;
-import ru.vsu.cs.dzhabbarov.cinema.repository.FilmRepository;
 
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-public class FilmService {
-    private final FilmRepository repository;
+public interface FilmService {
 
-    public List<FilmEntity> findFilmByName(String name) {
-        return repository.findAllByNameContainingIgnoreCase(name);
-    }
+    List<FilmEntity> findFilmByName(String name);
 
-    public Page<FilmEntity> getAllFilms(int offset, int limit) {
-        return repository.findAll(PageRequest.of(offset, limit, Sort.by("name")));
-    }
-
-
+    Page<FilmEntity> getAllFilms(int offset, int limit);
 }
---
+++ backend/src/main/java/ru/vsu/cs/dzhabbarov/cinema/service/FilmServiceImpl.java
@@ -0,0 +1,26 @@
+package ru.vsu.cs.dzhabbarov.cinema.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import ru.vsu.cs.dzhabbarov.cinema.entity.FilmEntity;
+import ru.vsu.cs.dzhabbarov.cinema.repository.FilmRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FilmServiceImpl implements FilmService {
+    private final FilmRepository repository;
+
+    public List<FilmEntity> findFilmByName(String name) {
+        return repository.findAllByNameContainingIgnoreCase(name);
+    }
+
+    public Page<FilmEntity> getAllFilms(int offset, int limit) {
+        return repository.findAll(PageRequest.of(offset, limit, Sort.by("name")));
+    }
+
+}
