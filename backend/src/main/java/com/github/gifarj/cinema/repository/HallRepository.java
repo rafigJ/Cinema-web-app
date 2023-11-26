dc18907bc0af1d4b7cccd607c9bf0f57f9cf21a9
Rafig <rafigdzabbarov0410@gmail.com>
2023-11-26 18:45:53 +0300

    backend big change other OneToMany Entities: Hall, Session, Ticket

--- backend/src/main/java/com/github/gifarj/cinema/repository/HallRepository.java
+++ backend/src/main/java/com/github/gifarj/cinema/repository/HallRepository.java
@@ -1,7 +1,6 @@
 package com.github.gifarj.cinema.repository;
 
 import com.github.gifarj.cinema.entity.HallEntity;
-import com.github.gifarj.cinema.entity.TicketEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HallRepository extends JpaRepository<HallEntity, Integer> {
