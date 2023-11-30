f9b675ea2a9025c6bfce626ab04d71da531b791e
Rafig <rafigdzabbarov0410@gmail.com>
2023-11-30 13:01:01 +0300

    frontend import Open Sans family, some FilmDetails style

--- frontend/src/components/UI/FilmDetailSection/FilmDetailsSection.jsx
+++ frontend/src/components/UI/FilmDetailSection/FilmDetailsSection.jsx
@@ -9,14 +9,14 @@ const FilmDetailsSection = ({id, name, year, poster, description, genreArray}) =
                  src={poster}
             />
             <div className="film-info">
-                <h1 className="film-info__title">{name}</h1>
+                <h1 className="film-info__title">{name} ({year} г.)</h1>
                 <div className="film-info__buttons"></div>
 
                 <section className="film-detail">
                     <h2 className="film-detail__title">О фильме</h2>
                     <span className="film-detail__year">Год производства: {year}</span>
 
-                    <span className="film-genres"> Жанры:
+                    <span className="film-genres">Жанр:{' '}
                         <ul className="film-genres__list">
                             {genreArray.map(genre =>
                                 <li key={genre.id}
