f46bb036d586c874d4b0749a4800930b03f5c4b0
Rafig <rafigdzabbarov0410@gmail.com>
2023-12-06 20:32:47 +0300

    backend fix: remove TestController, excess repository

--- backend/src/main/java/com/github/gifarj/cinema/repository/SessionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.github.gifarj.cinema.repository;
-
-import com.github.gifarj.cinema.entity.SessionEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SessionRepository extends JpaRepository<SessionEntity, Integer> {
-
-}
--- backend/src/main/java/com/github/gifarj/cinema/repository/TicketRepository.java
+++
@@ -1,8 +0,0 @@
-package com.github.gifarj.cinema.repository;
-
-import com.github.gifarj.cinema.entity.TicketEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketRepository extends JpaRepository<TicketEntity, Integer> {
-
-}
