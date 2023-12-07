f87ba5353b4068fc4e562d6d063132d100c7cc8b
Rafig <rafigdzabbarov0410@gmail.com>
2023-12-07 17:43:25 +0300

    backend fix: remove unused import

--- backend/src/main/java/com/github/gifarj/cinema/exception/RestExceptionHandler.java
+++ backend/src/main/java/com/github/gifarj/cinema/exception/RestExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.github.gifarj.cinema.exception;
 
 import com.github.gifarj.cinema.dto.RestExceptionDto;
-import com.github.gifarj.cinema.exception.RestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
