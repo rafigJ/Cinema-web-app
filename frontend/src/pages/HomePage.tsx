1b5609a9e09facbb1e0931efa0ebb1093ce51cba
Rafig <rafigdzabbarov0410@gmail.com>
2023-12-13 20:52:51 +0300

    frontend add: testing search

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {IFilm} from "../types/types";
+import {useFetching} from "../hooks/useFetching";
+import FilmService from "../API/FilmService";
+import FilmGrid from "../components/UI/FilmGrid/FilmGrid";
 
 const HomePage = () => {
+    const [films, setFilms] = useState([] as IFilm[])
+
+    const [fetchFilms, isLoading, error] = useFetching(async () => {
+        const response = await FilmService.getByName("Криминальное");
+        setFilms(response.data.content)
+    },)
+
+    useEffect(() => {
+        fetchFilms();
+    }, []);
+
+    if (isLoading) {
+        return <p>Загрузка...</p>
+    }
+
+    if (error !== '') {
+        return <p>{error}</p>
+    }
     return (
         <main className="main-page">
-            Nothing
+            <FilmGrid title="Криминальное..." films={films}/>
         </main>
     );
 };
